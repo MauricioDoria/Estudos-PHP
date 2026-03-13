e669e82ae64af8d52ec387089a67f1b27d6ea2f7
MauricioDoria <mur1kin44@gmail.com>
2026-03-13 16:36:37 -0300

    Update Average Grades.php

--- Average Grades.php
+++ Average Grades.php
@@ -1,23 +1,34 @@
 <?php 
 $media=0;
+echo 'Nome do Aluno: ' ; $aluno = trim(fgets(STDIN));
+if (is_numeric($aluno[0]) == true ){
+    echo "ERRRo!!! novamente...\n";
+    echo 'Nome do Aluno: ' ; $aluno = trim(fgets(STDIN));
+    
+}
+
 for ($i=1; $i < 5; $i++) {
-    echo "\nDigite a primeira a $i ª nota: " ; $nota = (int)trim(fgets(STDIN));
+    echo "\nDigite a primeira a $i ª nota: " ; $nota = (float)trim(fgets(STDIN));
     while (is_numeric($nota) == false){
-        echo "\nDigite a primeira a $i ª nota: " ; $nota = (int)trim(fgets(STDIN));
+        echo "\nDigite a primeira a $i ª nota: " ; $nota = (float)trim(fgets(STDIN));
     }
-    if ($nota <0 or $nota = null){
+    if ($nota <0 or $nota == null){
         $nota = 0;
     }
-    $media+=$nota;
-    echo $i;
+    $media += $nota;
+    echo $media;
 }
 $media /= 4;
 
 echo "A media do $aluno é de $media e ele foi  ";
-if ($media >= 6 ){
+if ($media >= 7 ){
     echo "APROVADO";
-} else{
+} elseif($media >= 5 and $media < 7 ){
+    echo "RECUPERAÇÃO";
+}else{
     echo "REPROVADO";
 }
+
+
 
 ?>
