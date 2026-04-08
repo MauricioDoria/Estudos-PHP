911d8e9e410dbeafbc72a8679ab8810d1193d719
MauricioDoria <mur1kin44@gmail.com>
2026-04-08 20:36:48 -0300

    Update Odd & Even.php

--- Odd & Even.php
+++ Odd & Even.php
@@ -2,9 +2,9 @@
 echo '---ODD & EVEN---' . PHP_EOL; #titulo
 
 $Number = []; $Even = []; $Odd = [];
-echo "Digite os numeros separandos por ' , ' :";$txt =trim(fgets(STDIN));
+echo "Enter numbers separated by ' , ': ";$txt =trim(fgets(STDIN));
 $Number = explode(',' , $txt);
-$Number = array_map('intval',$Number);
+$Number = array_map('intval',$Number);#transforma em Inteiro | Convert to integer
 
 foreach ($Number as $n){
     echo $n . PHP_EOL;
@@ -12,13 +12,13 @@ foreach ($Number as $n){
         $Even[] = $n;
     }else{$Odd[] = $n;}
 }
-echo 'Temos ' .count($Even) . ' numeros pares e ' . count($Odd) .' numreos impares...' . PHP_EOL;
+echo 'We have ' .count($Even) . ' even numbers and ' . count($Odd) .' odd numbers...' . PHP_EOL;
 foreach ($Even as $Even){
-    echo " $Even é Par ";
+    echo " $Even is Even ";
 }
 echo "\n...\n";
-foreach ($Odd as $Odd){
-    echo " $Odd é Impar ";
+foreach ($Odd as $Odd){                                
+    echo " $Odd is Odd ";
 }
 
 
