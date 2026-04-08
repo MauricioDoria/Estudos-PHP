2584d7f3bf9f6ab244cf6cbba868e22928617487
MauricioDoria <mur1kin44@gmail.com>
2026-04-08 20:09:28 -0300

    Update Multiplication Table.php
    Transcribed into English

--- Multiplication Table.php
+++ Multiplication Table.php
@@ -1,25 +1,26 @@
 <?php
-$mop = 'S';
-while ($mop == 'S'){ 
+$mop = 'Y';
+while ($mop == 'Y'){ 
 
-    echo "Qual é a sua opção: [0]DOIS NUMEROS [1]TABELA COMPLETA: ";$op = trim(fgets(STDIN));
+    echo "What is your choice: [0]Two Numbers [1]Complete Multiplication Table: ";$op = trim(fgets(STDIN));
     while (is_numeric($op) == false or $op > 1){
-        echo "Qual é a sua opção: [0]DOIS NUMEROS [1]TABELA COMPLETA: ";$op = trim(fgets(STDIN));
+        echo 'ERRO' . PHP_EOL;
+        echo "What is your choice: [0]Two Numbers [1]Complete Multiplication Table:  ";$op = trim(fgets(STDIN));
     }
 
-    echo PHP_EOL. 'Numero desejado: ';$n1=(int)trim(fgets(STDIN));
+    echo PHP_EOL. 'Desired Number: ';$n1=(int)trim(fgets(STDIN));
     if ($op == 0){
-        echo 'Multiplicado por : ';$n2 = (int)trim(fgets(STDIN));
-        echo "\nO resultado entre $n1 * $n2 = " . $n1*$n2;
+        echo 'Multiply by : ';$n2 = (int)trim(fgets(STDIN));
+        echo "\nThe result of $n1 * $n2 = " . $n1*$n2;
     }
     else{
         for($number = 0 ; $number <= 10; $number++)
             echo "$n1 * $number = ". $n1 * $number . PHP_EOL;
     }
 
-    echo 'Deseja continuar: '; $mop = strtoupper(trim(fgets(STDIN)));
-    while (is_numeric($mop) == true or $mop != 'S' and $mop != 'N'){
-        echo 'Deseja continuar: '; $mop = strtoupper(trim(fgets(STDIN)));
+    echo PHP_EOL . 'Do you wish to continue: [Y]Yes [N]No: 0'; $mop = strtoupper(trim(fgets(STDIN)));
+    while (is_numeric($mop) == true or $mop != 'Y' and $mop != 'N'){
+        echo PHP_EOL . 'Continue: '; $mop = strtoupper(trim(fgets(STDIN)));
     }
 
 }
