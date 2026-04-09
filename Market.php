932a25c79a706db1deb75996fa82d9f98e1e3410
MauricioDoria <mur1kin44@gmail.com>
2026-04-09 16:22:16 -0300

    Create Market.php

---
+++ Market.php
@@ -0,0 +1,37 @@
+<?php 
+$Vtotal = 0;
+$list = [
+    'feijão' => 5.50,
+    'arroz' => 7.00,
+    'cebola' => 2.50,
+    'tomate' => 3.00,
+    'café' => 8.90,
+    'oleo'=> 6.30,
+    'macarrão' => 2.90,
+    'leite' => 3.80,
+    'pão' => 4.00,
+    'açucar' => 4.20 
+];
+while (True){
+    echo 'Qual Produto: ';$product = strtolower(trim(fgets(STDIN)));
+    
+    if ($list[$product] == true){
+        echo PHP_EOL . 'Unidades: '; $uni = trim(fgets(STDIN));
+        $Vtotal += $list[$product] * $uni;
+    }
+    else {
+        echo PHP_EOL. 'Não esta em';
+    }
+    echo "\nPARAR [s]";$option = strtoupper(trim(fgets(STDIN)));
+    if ($option[0] == 'S'){
+        break;
+    }
+    
+    echo "\n$Vtotal\n";
+}
+echo "Valor total da compra foi de $Vtotal";
+
+
+
+
+?>
