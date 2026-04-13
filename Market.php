8141f28990a40783dfd17d815a6845aac018c709
MauricioDoria <mur1kin44@gmail.com>
2026-04-13 18:25:53 -0300

    Update Market.php

--- Market.php
+++ Market.php
@@ -1,16 +1,16 @@
 <?php 
 $Vtotal = 0;
 $list = [
-    'feijão' => 5.50,
-    'arroz' => 7.00,
-    'cebola' => 2.50,
-    'tomate' => 3.00,
-    'café' => 8.90,
-    'oleo'=> 6.30,
-    'macarrão' => 2.90,
-    'leite' => 3.80,
-    'pão' => 4.00,
-    'açucar' => 4.20 
+    'bean' => 5.50,
+    'rice' => 7.00,
+    'onion' => 2.50,
+    'tomato' => 3.00,
+    'coffe' => 8.90,
+    'oil'=> 6.30,
+    'noodle' => 2.90,
+    'milk' => 3.80,
+    'bread' => 4.00,
+    'sugar' => 4.20 
 ];
 while (True){
     echo 'Product: ';$product = strtolower(trim(fgets(STDIN)));
@@ -22,14 +22,14 @@ while (True){
     else {
         echo PHP_EOL. 'Not Available';
     }
-    echo "\nPARAR [s]";$option = strtoupper(trim(fgets(STDIN)));
-    if ($option[0] == 'S'){
+    echo "\nSTOP [Y]";$option = strtoupper(trim(fgets(STDIN)));
+    if ($option[0] == 'Y'){
         break;
     }
     
     echo "\n$Vtotal\n";
 }
-echo "Valor total da compra foi de $Vtotal";
+echo "Total Purchase Prince: $Vtotal";
 
 
 
