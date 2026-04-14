faea321b5a34fc2ac390ff0ea644e032faf140df
MauricioDoria <mur1kin44@gmail.com>
2026-04-14 20:40:42 -0300

    Upgrade

---
+++ Multiplication_Table.php
@@ -0,0 +1,27 @@
+<?php
+$mop = 'Y';
+while ($mop == 'Y'){ 
+
+    echo "What is your choice: [0]Two Numbers [1]Complete Multiplication Table: ";$op = trim(fgets(STDIN));
+    while (is_numeric($op) == false or $op > 1){
+        echo 'ERRO' . PHP_EOL;
+        echo "What is your choice: [0]Two Numbers [1]Complete Multiplication Table:  ";$op = trim(fgets(STDIN));
+    }
+
+    echo PHP_EOL. 'Desired Number: ';$n1=(int)trim(fgets(STDIN));
+    if ($op == 0){
+        echo 'Multiply by : ';$n2 = (int)trim(fgets(STDIN));
+        echo "\nThe result of $n1 * $n2 = " . $n1*$n2;
+    }
+    else{
+        for($number = 0 ; $number <= 10; $number++)
+            echo "$n1 * $number = ". $n1 * $number . PHP_EOL;
+    }
+
+    echo PHP_EOL . 'Do you wish to continue: [Y]Yes [N]No: 0'; $mop = strtoupper(trim(fgets(STDIN)));
+    while (is_numeric($mop) == true or $mop != 'Y' and $mop != 'N'){
+        echo PHP_EOL . 'Continue: '; $mop = strtoupper(trim(fgets(STDIN)));
+    }
+
+}
+
---
+++ Odd&Even.php
@@ -0,0 +1,23 @@
+<?php 
+echo '---ODD & EVEN---' . PHP_EOL; #titulo
+
+$Number = []; $Even = []; $Odd = [];
+echo "Enter numbers separated by ' , ': ";$txt =trim(fgets(STDIN));
+$Number = explode(',' , $txt);
+$Number = array_map('intval',$Number);#transforma em Inteiro | Convert to integer
+
+foreach ($Number as $n){
+    echo $n . PHP_EOL;
+    if ($n % 2 == 0){
+        $Even[] = $n;
+    }else{$Odd[] = $n;}
+}
+echo 'We have ' .count($Even) . ' even numbers and ' . count($Odd) .' odd numbers...' . PHP_EOL;
+foreach ($Even as $Even){
+    echo " $Even is Even ";
+}
+echo "\n...\n";
+foreach ($Odd as $Odd){                                
+    echo " $Odd is Odd ";
+}
+
