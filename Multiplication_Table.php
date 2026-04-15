097d30e076fb24b391681af65c1a5eb1597df9d3
MauricioDoria <mur1kin44@gmail.com>
2026-04-15 18:45:24 -0300

    Update Multiplication_Table.php

--- Multiplication_Table.php
+++ Multiplication_Table.php
@@ -2,20 +2,20 @@
 $MenuOption = 'Y';
 while ($MenuOption == 'Y'){ 
 
-    echo "What is your choice: [0]Two Numbers [1]Complete Multiplication Table: ";$op = trim(fgets(STDIN));
-    while (is_numeric($op) == false or $op > 1){
+    echo "What is your choice: [0]Two Numbers [1]Complete Multiplication Table: ";$Option = trim(fgets(STDIN));
+    while (is_numeric($Option) == false or $Option > 1){
         echo 'ERRO' . PHP_EOL;
-        echo "What is your choice: [0]Two Numbers [1]Complete Multiplication Table:  ";$op = trim(fgets(STDIN));
+        echo "What is your choice: [0]Two Numbers [1]Complete Multiplication Table:  ";$Option = trim(fgets(STDIN));
     }
 
-    echo PHP_EOL. 'Desired Number: ';$n1=(int)trim(fgets(STDIN));
-    if ($op == 0){
-        echo 'Multiply by : ';$n2 = (int)trim(fgets(STDIN));
-        echo "\nThe result of $n1 * $n2 = " . $n1*$n2;
+    echo PHP_EOL. 'Desired Number: ';$Number1=(int)trim(fgets(STDIN));
+    if ($Option == 0){
+        echo 'Multiply by : ';$Number2 = (int)trim(fgets(STDIN));
+        echo "\nThe result of $Number1 * $Number2 = " . $Number1*$Number2;
     }
     else{
         for($number = 0 ; $number <= 10; $number++)
-            echo "$n1 * $number = ". $n1 * $number . PHP_EOL;
+            echo "$Number1 * $number = ". $Number1 * $number . PHP_EOL;
     }
 
     echo PHP_EOL . 'Do you wish to continue: [Y]Yes [N]No: 0'; $MenuOption = strtoupper(trim(fgets(STDIN)));
